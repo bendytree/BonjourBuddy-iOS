a368371043c2d3ddd51458bff4a88db88f521d60
Josh Wright <help@bendytree.com>
2013-05-20 19:05:32 -0500

    added example project

---
+++ BonjourBuddyExample/BonjourBuddyExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BonjourBuddyExample
+//
+//  Created by Joshua Wright on 5/20/13.
+//  Copyright (c) 2013 Joshua Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BonjourBuddyExample/BonjourBuddyExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BonjourBuddyExample
+//
+//  Created by Joshua Wright on 5/20/13.
+//  Copyright (c) 2013 Joshua Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
